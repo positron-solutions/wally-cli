ff482cad3026a6024366ac456a3c60a9674ec4e2
Psionik K <73710933+psionic-k@users.noreply.github.com>
2020-12-01 11:05:19 +0800

    Convert to vgo2nix generation of deps.nix

--- default.nix
+++ default.nix
@@ -2,26 +2,21 @@
 
 buildGoPackage {
   name = "wally-cli";
+  goPackagePath = "github.com/zsa/wally-cli";
 
-  goPackagePath = "github.com/zsa/wally";
-  subPackages = [ "cli" ];
   src = fetchFromGitHub {
     owner = "zsa";
-    repo = "wally";
-    rev = "311d39c33efcde5fde5f6ad79298258584f42ece";
-    sha256 = "sha256:1hz5zzjffs90r720lqkchi6zfyz7gq7lpsmyf5psfa7l708nvayk";
+    repo = "wally-cli";
+    rev = "5c536c2c4ca974fb83495d040e69da4a8051eaa3";
+    sha256 = "07spp3mvkqp0grn2hwgsghrw9136i0v2rxyf8zymvdqniwlabad5";
   };
-  patches = [ ./cli-only.patch ];
+
   nativeBuildInputs = with pkgs; [ pkg-config ];
   buildInputs = with pkgs; [ libusb ];
   goDeps = ./deps.nix;
 
-  postInstall = ''
-    mv $out/bin/cli $out/bin/wally-cli
-  '';
-
   meta = with lib; {
-    description = "Upload ergodox CLI keyboard ROM's";
+    description = "Upload Ergodox keyboard ROM's";
     license = licenses.mit;
     homepage = https://github.com/zsa/wally;
   };
--- deps.nix
+++ deps.nix
@@ -1,65 +1,65 @@
-# file generated from Gopkg.lock using dep2nix (https://github.com/nixcloud/dep2nix)
+# file generated from go.mod using vgo2nix (https://github.com/adisbladis/vgo2nix)
 [
   {
-    goPackagePath  = "github.com/caarlos0/spin";
+    goPackagePath = "github.com/caarlos0/spin";
     fetch = {
       type = "git";
       url = "https://github.com/caarlos0/spin";
-      rev =  "ac25467273ecdf236d2d83a25c5329d5863d375f";
+      rev = "v1.1.0";
       sha256 = "1pnijds4145j8nsxvq8203r2sg2pbk7x8prkdg2ilghhrzqj6vyc";
     };
   }
   {
-    goPackagePath  = "github.com/google/gousb";
+    goPackagePath = "github.com/google/gousb";
     fetch = {
       type = "git";
       url = "https://github.com/google/gousb";
-      rev =  "18f4c1d8a750878c4f86ac3d7319b8aa462a79f9";
-      sha256 = "1i7ffkq395x0v1186j9bhfgy67wlq6s840xkyx8wn87w55yd05rh";
+      rev = "v2.1.0";
+      sha256 = "1aki6hk009sicrf7gxy5nkjmj4j7lsy0by4kjgd9bwq8ragfyv5x";
     };
   }
   {
-    goPackagePath  = "github.com/logrusorgru/aurora";
+    goPackagePath = "github.com/logrusorgru/aurora";
     fetch = {
       type = "git";
       url = "https://github.com/logrusorgru/aurora";
-      rev =  "21d75270181e0436fee7bd58b991c212cf309068";
-      sha256 = "0vc9qdl6jzq7vazfqgz628gcgsvir56bdi2bkhl54pi92cz9cw0p";
+      rev = "v2.0.3";
+      sha256 = "1ck2j2ff2avph07vgq0r1y7hmbqgvk339rvph45dcwgci23lb3pf";
     };
   }
   {
-    goPackagePath  = "github.com/marcinbor85/gohex";
+    goPackagePath = "github.com/marcinbor85/gohex";
     fetch = {
       type = "git";
       url = "https://github.com/marcinbor85/gohex";
-      rev =  "7a43cd876e46e0f6ddc553f10f91731a78e6e949";
-      sha256 = "1nzy1hk7blawq4skycj330ac3kfwh0xr6xd7s3y09hsxfzcv4p9v";
+      rev = "baab2527a9a2";
+      sha256 = "06v4cc6ld6vvxd4xm9k6l49lhcd9ncq7xfx35mj5b9r96ih49fiz";
     };
   }
   {
-    goPackagePath  = "github.com/mattn/go-runewidth";
+    goPackagePath = "github.com/mattn/go-runewidth";
     fetch = {
       type = "git";
       url = "https://github.com/mattn/go-runewidth";
-      rev =  "18c3d09a134a52720932bbaa92c798a0ab111004";
-      sha256 = "1snr8mk63vz2h44knq26dm81p83887v7kb09iywqmx0nqzngih66";
+      rev = "v0.0.9";
+      sha256 = "1mvlxcdwr0vwp8b2wqs6y7hk72y28sqh03dz5x0xkg48d4y9cplj";
     };
   }
   {
-    goPackagePath  = "golang.org/x/sys";
+    goPackagePath = "golang.org/x/sys";
     fetch = {
       type = "git";
       url = "https://go.googlesource.com/sys";
-      rev =  "548cf772de5052aa878ccb47cdeb7d262b75c8ec";
-      sha256 = "1r8mbj5v58hx6090nq2hrngxzdd3a1vjpzy7mbh28rzpbpl16klj";
+      rev = "d9f96fdee20d";
+      sha256 = "1lj8nmnpb7ngbd90il65kyh78i6sc325vhmxr6l61qvbh0qdybyh";
     };
   }
   {
-    goPackagePath  = "gopkg.in/cheggaaa/pb.v1";
+    goPackagePath = "gopkg.in/cheggaaa/pb.v1";
     fetch = {
       type = "git";
-      url = "https://github.com/cheggaaa/pb";
-      rev =  "f907f6f5dd81f77c2bbc1cde92e4c5a04720cb11";
+      url = "https://gopkg.in/cheggaaa/pb.v1";
+      rev = "v1.0.28";
       sha256 = "13a66cqbpdif804qj12z9ad8r24va9q41gfk71qbc4zg1wsxs3rh";
     };
   }
